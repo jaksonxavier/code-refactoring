0c5e2574ba78cb4aa8d50bcd5c2e4c1e52ccd8d5
Jakson Xavier <jackd.xavier@gmail.com>
2022-07-27 16:24:06 -0300

    refactor(extract function): move switch case to specific function

--- statement.mjs
+++ statement.mjs
@@ -10,25 +10,7 @@ export default function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     // soma créditos por volume
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -49,4 +31,28 @@ export default function statement(invoice, plays) {
   result += `You earned ${volumeCredits} credits\n`;
 
   return result;
+}
+
+function amountFor(performance, play) {
+  let amount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      amount = 40000;
+      if (performance.audience > 30) {
+        amount += 1000 * (performance.audience - 30);
+      }
+      break;
+    case "comedy":
+      amount = 30000;
+      if (performance.audience > 20) {
+        amount += 10000 + 500 * (performance.audience - 20);
+      }
+      amount += 300 * performance.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return amount;
 }
