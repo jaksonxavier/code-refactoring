e680387f5c58cfd7c4b3a0642e7060b603801187
Jakson Xavier <jackd.xavier@gmail.com>
2022-07-27 15:43:45 -0300

    feat: code base

---
+++ main.mjs
@@ -0,0 +1,8 @@
+import statement from "./statement.mjs";
+
+import invoices from "./invoices.json" assert { type: "json" };
+import plays from "./plays.json" assert { type: "json" };
+
+for (const invoice of invoices) {
+  console.log(statement(invoice, plays));
+}
