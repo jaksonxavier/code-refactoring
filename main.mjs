ae6eae411f93c9ec8c1c9765846c7b5d7a59257f
Jakson Xavier <jackd.xavier@gmail.com>
2022-07-28 02:55:03 -0300

    feat: generate files with report

--- main.mjs
+++ main.mjs
@@ -1,8 +1,24 @@
-import statement from "./src/statement.mjs";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import invoices from "./src/dataset/invoices.json" assert { type: "json" };
 import plays from "./src/dataset/plays.json" assert { type: "json" };
+import statement from "./src/statement.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const [, , reportType = "plainText", filename = "report.txt"] = process.argv;
 
 for (const invoice of invoices) {
-  console.log(statement(invoice, plays));
+  try {
+    fs.writeFileSync(
+      path.join(__dirname, "reports", filename),
+      statement(invoice, plays, reportType)
+    );
+
+    console.log("✅ - Report generated successfully");
+  } catch (error) {
+    console.error("❌ - " + error.message);
+  }
 }
