ccd170367bfe4915cb4c7f89fde8901520bcb1fc
Jakson Xavier <jackd.xavier@gmail.com>
2022-07-28 02:05:27 -0300

    refactor: move dataset's to its own folder

--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
 import statement from "./src/statement.mjs";
 
-import invoices from "./invoices.json" assert { type: "json" };
-import plays from "./plays.json" assert { type: "json" };
+import invoices from "./src/dataset/invoices.json" assert { type: "json" };
+import plays from "./src/dataset/plays.json" assert { type: "json" };
 
 for (const invoice of invoices) {
   console.log(statement(invoice, plays));
