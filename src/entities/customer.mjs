5258193a846866940359cbe6c3151895bc316484
Jakson Xavier <jackd.xavier@gmail.com>
2022-08-08 04:43:14 -0300

    refactor: update customer entity

--- src/entities/customer.mjs
+++ src/entities/customer.mjs
@@ -1,10 +1,37 @@
-import { randomUUID } from "node:crypto";
+import BaseEntity from "./base-entity.mjs";
 
-export default class Customer {
-  constructor({ name, email, phoneNumber }) {
-    this.id = randomUUID();
-    this.name = name;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
+export default class Customer extends BaseEntity {
+  constructor(props, id) {
+    super(props, id);
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name) {
+    this.props.name = name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  set email(email) {
+    this.props.email = email;
+  }
+
+  get phoneHumber() {
+    return this.props.phoneHumber;
+  }
+
+  set phoneHumber(phoneHumber) {
+    this.props.phoneHumber = phoneHumber;
+  }
+
+  create(props, id) {
+    const customer = new Customer(props, id);
+
+    return customer;
   }
 }
